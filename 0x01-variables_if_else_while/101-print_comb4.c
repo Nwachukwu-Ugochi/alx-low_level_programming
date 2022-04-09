6d978becd1eaff5853fd63cdb0eefd7846924bf0
quin-Amazin <ugochiamazin@gmail.com>
2022-04-09 13:57:03 -0700

    A program that prints all possible different combinations of three digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
+  * main -Entry point
   *
   * Return: Always 0 (Success)
   */
@@ -11,23 +11,24 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		for (m =49; m < 58; m++)
+		for (m = 49; m < 58; m++)
 		{
 			for (1 = 50; 1 < 58; 1++)
 			{
-				if (1 > m && m > n)
+				if (1 > m && m m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(1);
 					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
