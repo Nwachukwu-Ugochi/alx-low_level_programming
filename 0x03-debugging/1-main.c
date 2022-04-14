215b324261f4456ff2bfd561f642b8623588e5dd
quin-Amazin <ugochiamazin@gmail.com>
2022-04-13 18:52:57 -0700

    updated Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,10 @@ int main(void)
 	i = 0;
 
 	/*
-	   *while (i < 10)
+	   * while (i < 10)
 	   *{
-	   *putchar(i);
+	   * Infinite loop - No increase of variable
+	   * putchar(i);
 	   }
 	 */
 	printf("Infinite loop avided! \\o/\n");
